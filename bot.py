d7370a53e284503dc988fbcc1a5fa9cf790a4f77
HarshuXD <95332135+HarshuXD@users.noreply.github.com>
2022-03-14 13:37:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def HelpWatermark(bot, cmd):
 	await cmd.reply_text(
 		text=Config.USAGE_WATERMARK_ADDER,
 		parse_mode="Markdown",
-		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")], [InlineKeyboardButton("Source Code", url="https://github.com/AbirHasan2005/Watermark-Bot")]]),
+		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/Harshu_xD"), InlineKeyboardButton("Support", url="https://t.me/Harshu_xD")], [InlineKeyboardButton("Powered By", url="https://t.me/StarterMods")], [InlineKeyboardButton("Movie Group", url="https://t.me/StarterMovie")]]),
 		disable_web_page_preview=True
 	)
 
@@ -418,7 +418,7 @@ async def button(bot, cmd: CallbackQuery):
 				return
 			except Exception:
 				await cmd.message.edit(
-					text="Something went Wrong. Contact my [Support Group](https://t.me/DevsZone).",
+					text="Something went Wrong. Contact my [ Boss ](https://t.me/Harshu_xD).",
 					parse_mode="markdown",
 					disable_web_page_preview=True
 				)
@@ -426,7 +426,7 @@ async def button(bot, cmd: CallbackQuery):
 		await cmd.message.edit(
 			text=Config.USAGE_WATERMARK_ADDER,
 			parse_mode="Markdown",
-			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/harshu_xd"), InlineKeyboardButton("Support", url="https://t.me/Harshu_xD")], [InlineKeyboardButton("Powered By", url="https://t.me/StarterMods")]]),
 			disable_web_page_preview=True
 		)
 
